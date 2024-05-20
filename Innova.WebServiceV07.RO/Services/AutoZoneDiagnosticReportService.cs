908f79921b27d1e6d99d905e4eb21a26020cd560
hoangdx <daoxh@Carmd.com>
2024-05-20 16:44:40 +0700

    add log file when push queue payload error

--- Innova.WebServiceV07.RO/Services/AutoZoneDiagnosticReportService.cs
+++ Innova.WebServiceV07.RO/Services/AutoZoneDiagnosticReportService.cs
@@ -1,8 +1,10 @@
 ﻿using Innova.WebServiceV07.RO.DataModels.RabbitMQModels;
 using Innova.WebServiceV07.RO.DataModels.ServiceV6Models.LogDiagnosticReport;
+using Innova.WebServiceV07.RO.Helpers;
 using Innova.WebServiceV07.RO.RabbitMQPublishers;
 using Microsoft.Practices.EnterpriseLibrary.Logging;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Innova.WebServiceV07.RO.Services
@@ -25,6 +27,23 @@ namespace Innova.WebServiceV07.RO.Services
             if (!isOk)
             {
                 Logger.Write($"RO AutoZoneBlackboxLogging => Push Payload Log To RabbitMQ Failed => queueName: {Global.RabbitMQ_QueueName_AutoZoneDiagnosticReportLogging} => Exception: {rabbitMQHandlerEx}");
+
+                #region Save payload to file
+
+                var payloadInfo = model.PayloadInfo;
+
+                if (payloadInfo.Any())
+                {
+                    string externalSystemName = model.ExternalSystemName;
+
+                    string faliedFileName = $"{externalSystemName}_Payload_" + DateTime.Now.ToString("yyyyMMdd HHmmss.fffffff") + "_" + Guid.NewGuid() + ".txt";
+
+                    Logger.Write($"Send Request To RabbitMQ Failed => SavePayloadToFile => externalSystemName: {externalSystemName} => faliedFileName: {faliedFileName}");
+
+                    PayloadHelper.SavePayloadToFile(externalSystemName, Global.ServiceReadOnly_NewPayloadFolder_When_SendRequest_ToRabbitMQ_Failed, faliedFileName, payloadInfo);
+                }
+
+                #endregion Save payload to file
             }
         }
     }
